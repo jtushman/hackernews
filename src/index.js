b4e6fad07b5fdc9526b7c668c933b8f902d2ddba
Jonathan Tushman <jtushman@gmail.com>
2018-07-25 13:04:37 +0200

    hackernews example

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+
+const resolvers = {
+    Query,
+    Mutation,
+    AuthPayload,
+    Subscription,
+    Feed,
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/jonathan-tushman-4f8f5d/hackernews/dev',
+            secret: 'mysecret123',
+            debug: true,
+        })
+    })
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
